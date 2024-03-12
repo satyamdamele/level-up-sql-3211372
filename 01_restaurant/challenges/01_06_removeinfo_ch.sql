8408c5d16f2d9c0eccb8df9deb71c4e427f5c923
satyamdamele <69321258+satyamdamele@users.noreply.github.com>
2024-03-12 12:32:50 +0000

    Completed challenge 6 and 7 - restaurant section, library section not started yet.

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,15 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT CustomerID FROM Customers 
+WHERE FirstName = 'Norby';
+
+SELECT * FROM Reservations 
+WHERE CustomerID = 64 AND Date > '2022-07-24'; 
+
+DELETE FROM Reservations
+WHERE ReservationID = 2000;
+
+SELECT * FROM Reservations 
+WHERE CustomerID = 64;
--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,10 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+SELECT CustomerID FROM Customers 
+WHERE Email = 'atapley2j@kinetecoinc.com';
+
+INSERT INTO InvitationResponses
+VALUES (1, 92, 3);
+
