db0f02fb604122e095efb6099046cb8fcda66fd6
satyamdamele <69321258+satyamdamele@users.noreply.github.com>
2024-03-13 19:02:53 +0000

    Library 7 and completed. Final commit.

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,15 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT Published AS Year, Count(DISTINCT Title) AS 'No of books'
+FROM Books
+GROUP BY Published
+ORDER BY Count(DISTINCT Title) DESC;
+
+SELECT Books.Title AS 'Most popular books'
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+GROUP BY (Books.Title)
+ORDER BY Count(Books.BookID) DESC
+LIMIT 5;
